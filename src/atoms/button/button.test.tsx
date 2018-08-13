4d6ed6fdc8b3f05b92dfe1de375563d044628993
Paul Scanlon <pauliescanlon@gmail.com>
2018-08-13 09:31:44 +0100

    attempting to get jest/enzyme working with emotion

---
+++ src/atoms/button/button.test.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+
+import { Button } from "./";
+
+describe("<Button />", () => {
+  it("renders correctly", () => {
+    const wrapper = shallow(<Button>Hooray</Button>);
+    expect(wrapper).toHaveLength(1);
+  });
+});
