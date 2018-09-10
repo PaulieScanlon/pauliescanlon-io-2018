e1c8c6a133d3ea565bdea53988e60ae2908b76c9
Paul Scanlon <pauliescanlon@gmail.com>
2018-09-10 20:53:37 +0100

    conditional ga initilize

--- src/utils/google-analytics.ts
+++ src/utils/google-analytics.ts
@@ -1,15 +1,16 @@
 import ReactGA from "react-ga";
 
 export const logPageView = (location: any) => {
-  if ((window as any) && !(window as any)["GoogleAnalyticsObject"]) {
-    ReactGA.initialize(`${process.env.GA_KEY}`, {
-      debug: false
-    });
+  if (window as any) {
+    if (!(window as any)["GoogleAnalyticsObject"]) {
+      ReactGA.initialize(`${process.env.GA_KEY}`, {
+        debug: false
+      });
+    }
+    // console.log("process.env.GA_KEY: ", process.env.GA_KEY);
+    // console.log("logPageView", location.pathname + location.search);
+    // console.log("pathname: ", location.pathname, " | search: ", location.search);
+    ReactGA.set({ page: location.pathname + location.search });
+    ReactGA.pageview(location.pathname + location.search);
   }
-
-  // console.log("process.env.GA_KEY: ", process.env.GA_KEY);
-  // console.log("logPageView", location.pathname + location.search);
-  // console.log("pathname: ", location.pathname, " | search: ", location.search);
-  ReactGA.set({ page: location.pathname + location.search });
-  ReactGA.pageview(location.pathname + location.search);
 };
