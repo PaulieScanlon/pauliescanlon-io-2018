1154f1bde883f884e9e27d94f3bc2fdd6aaf1b15
Paul Scanlon <pauliescanlon@gmail.com>
2018-09-10 13:50:32 +0100

    catching duplicat GA initialize

--- src/utils/google-analytics.ts
+++ src/utils/google-analytics.ts
@@ -1,9 +1,12 @@
 import ReactGA from "react-ga";
-ReactGA.initialize(`${process.env.GA_KEY}`, {
-  debug: false
-});
 
 export const logPageView = (location: any) => {
+  if (!(window as any)["GoogleAnalyticsObject"]) {
+    ReactGA.initialize(`${process.env.GA_KEY}`, {
+      debug: true
+    });
+  }
+
   // console.log("process.env.GA_KEY: ", process.env.GA_KEY);
   // console.log("logPageView", location.pathname + location.search);
   // console.log("pathname: ", location.pathname, " | search: ", location.search);
