2147c1c2f02ee6f9cd76d4ac5bbf6e3ba22accf1
Paul Scanlon <pauliescanlon@gmail.com>
2018-09-12 11:55:11 +0100

    only fire ga if NODE_ENV is in production

--- src/utils/google-analytics.ts
+++ src/utils/google-analytics.ts
@@ -1,6 +1,6 @@
 export const pageView = () => {
-  console.log("pageView: process.env.NODE_ENV: ", process.env.NODE_ENV);
-  if (process.env.NODE_ENV !== "development") {
+  // console.log("pageView: process.env.NODE_ENV: ", process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "production") {
     (window as any).gtag("config", `${process.env.GA_KEY}`, {
       page_location: location.pathname + location.search
     });
